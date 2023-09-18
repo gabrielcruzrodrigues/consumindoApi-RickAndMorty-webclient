4099042a573f599a0383c56bb6c6f4044453f6fc
unknown <contatogabrielcruzrodrigues@gmail.com>
2023-09-18 11:07:06 -0300

    Creating ListOfEpisodes endpoint

--- src/main/java/com/gabriel/rickAndMorty/client/RickAndMortyClient.java
+++ src/main/java/com/gabriel/rickAndMorty/client/RickAndMortyClient.java
@@ -2,12 +2,14 @@ package com.gabriel.rickAndMorty.client;
 
 import com.gabriel.rickAndMorty.response.CharacterResponse;
 import com.gabriel.rickAndMorty.response.EpisodeResponse;
+import com.gabriel.rickAndMorty.response.ListOfEpisodesResponse;
 import com.gabriel.rickAndMorty.response.LocationResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -56,5 +58,18 @@ public class RickAndMortyClient {
                 .onStatus(HttpStatusCode::is4xxClientError,
                         error -> Mono.error(new RuntimeException("Verifique os parametros informados")))
                 .bodyToMono(EpisodeResponse.class);
+    }
+
+    public Flux<ListOfEpisodesResponse> getAllEpisodes() {
+        log.info("Listando todos os episodios");
+        return webClient
+                .get()
+                .uri("/episode")
+                .accept(APPLICATION_JSON)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError,
+                        error -> Mono.error(new RuntimeException("Verifique os parametros informados")))
+                .bodyToFlux(ListOfEpisodesResponse.class);
+
     }
 }
--- src/main/java/com/gabriel/rickAndMorty/controller/RickAndMorthController.java
+++ src/main/java/com/gabriel/rickAndMorty/controller/RickAndMorthController.java
@@ -3,12 +3,14 @@ package com.gabriel.rickAndMorty.controller;
 import com.gabriel.rickAndMorty.client.RickAndMortyClient;
 import com.gabriel.rickAndMorty.response.CharacterResponse;
 import com.gabriel.rickAndMorty.response.EpisodeResponse;
+import com.gabriel.rickAndMorty.response.ListOfEpisodesResponse;
 import com.gabriel.rickAndMorty.response.LocationResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -33,5 +35,8 @@ public class RickAndMorthController {
         return rickAndMortyClient.findAnEpisodeById(id);
     }
 
-
+    @GetMapping("/episodes")
+    public Flux<ListOfEpisodesResponse> getAllEpisodes() {
+        return rickAndMortyClient.getAllEpisodes();
+    }
 }
